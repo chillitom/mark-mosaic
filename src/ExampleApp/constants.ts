393fed87512a09c626851127986f1c900e8442ff
Tom Rathbone <tom.rathbone@gmail.com>
2023-10-04 23:11:51 +0200

    initial work

---
+++ src/ExampleApp/constants.ts
@@ -0,0 +1,8 @@
+import classNames from 'classnames';
+import { Classes } from '@blueprintjs/core';
+
+export const THEMES = {
+    ['Blueprint']: 'mosaic-blueprint-theme',
+    ['Blueprint Dark']: classNames('mosaic-blueprint-theme', Classes.DARK),
+    ['None']: '',
+  };
